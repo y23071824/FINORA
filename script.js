125d78aa16be9eb613bd34c06e53d08318588c4d
y23071824 <y23071824@gmail.com>
2025-06-05 17:00:35 +0800

    Update script.js

--- script.js
+++ script.js
@@ -56,8 +56,8 @@ async function fetchExchangeRates() {
         "USD": 1,
         "TWD": 32,
         "JPY": 155,
-        "EUR": 1.08
-        "CNY": 7.18
+        "EUR": 1.08,
+        "CNY": 7.18,
       };
       console.log("📦 使用預設匯率資料", exchangeRates);
     }
